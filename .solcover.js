d7df2f516ba0e27ca7071bb10839b46675f99045
vittominacori <vittoriominacori@gmail.com>
2019-05-18 17:24:42 +0200

    fix solcover

--- .solcover.js
+++ .solcover.js
@@ -3,6 +3,8 @@ module.exports = {
     testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
     compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
     copyPackages: [
+        'erc-payable-token',
+        'eth-token-recover',
         'openzeppelin-solidity',
     ],
     skipFiles: [
