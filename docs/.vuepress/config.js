b46a97f5b38a8778e7b0e26dde9161a9b5d5e829
vittominacori <vittoriominacori@gmail.com>
2020-07-01 00:43:06 +0200

    update dependencies

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,11 @@ module.exports = {
   title: 'ICO Maker',
   description: 'Smart Contracts to build your ICO solution and issue your ERC20 Token',
   base: '/ico-maker/',
-  ga: 'UA-115756440-2',
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-115756440-2'
+    }]
+  ],
   themeConfig: {
     repo: 'vittominacori/ico-maker',
     sidebar: 'auto',
